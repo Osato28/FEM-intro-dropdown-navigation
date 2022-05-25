9a5787e55967eb8663a85032f5d58258cd4b9be4
Osato28 <65938107+Osato28@users.noreply.github.com>
2022-05-25 16:01:39 +0300

    Add basic JS/CSS toggle dropdown menu functionality for mobile

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// onClick processor
+function toggle(element) {
+    const toggledClass = element.dataset.classToggled ?? null;
+
+    (toggledClass && element?.classList.contains(toggledClass))
+    ? element.classList.remove(toggledClass)
+    : element.classList.add(toggledClass);
+}
+
+function untoggleAll(element, toggledClass) {
+    const toggledChildren = element.getElementsByClassName(toggledClass);
+    for (const el of toggledChildren) {
+        el?.classList.remove(toggledClass);
+    }
+}
+
+toggleElements = document.getElementsByClassName('toggle');
+
+document.addEventListener("click", (ev) => {
+    console.log("target: ", ev.target);
+
+    let targetFound = false;
+    for (el of toggleElements) {
+        // Check if the onClick's target is an immediate child of any elements that have .toggle class
+        if (el.firstElementChild === ev.target) {
+            console.log("container: ", el);
+            toggle(el);
+        }
+        if (el.contains(ev.target)) {
+            targetFound = true;
+        }
+    }
+
+    if (!targetFound) {
+        untoggleAll(document, "toggled");
+    }
+})
